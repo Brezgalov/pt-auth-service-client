4424c61fa448ab3e55e10b4488ceacecbbe568cf
Oleg Brezgalov <brezgalov.developer@gmail.com>
2022-08-17 13:16:17 +0300

    model errors formatter

---
+++ src/DataFormatters/ModelErrorsDataFormatter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Brezgalov\AuthServiceClient\DataFormatters;
+
+/**
+ * Class ModelErrorsDataFormatter
+ * @package Brezgalov\AuthServiceClient\DataFormatters
+ *
+ * Keep this class for single responsibility.
+ * AuthService error format changes will affect this class, not base one
+ */
+class ModelErrorsDataFormatter extends \Brezgalov\BaseApiClient\DataFormatters\ModelErrorsDataFormatter
+{
+}
